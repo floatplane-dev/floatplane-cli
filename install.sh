87b359ac557c70d223ceacb4f9cdf9b4973c5382
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-12-17 18:55:46 +1000

    Added installation script

--- install.sh
+++ install.sh
@@ -2,9 +2,26 @@
 
 set -e
 
-path=/usr/local/lib/fp/
+lib_path=/usr/local/lib/fp
+bin_path=/usr/local/bin/fp
+user=$(id -un)
 
-rm -rf $path
-mkdir $path
-cp -r lib/ $path
-ln -s $path/bin/fp /usr/local/bin/fp
+echo '---'
+echo 'installing Floatplane CLI ...'
+echo '---'
+echo "user: $user"
+echo '---'
+
+(
+  set -x
+  sudo rm -rf $lib_path
+  sudo rm -rf $bin_path
+  sudo mkdir $lib_path
+  sudo chown $user:staff $lib_path
+  cp -r ./lib/ $lib_path
+  sudo ln -s $lib_path/bin/fp $bin_path
+)
+
+echo '---'
+echo 'done!'
+echo '---'
