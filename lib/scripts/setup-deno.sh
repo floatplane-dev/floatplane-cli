a93a0243041283ac1ba3f11cd42d7d5d842899e2
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-04-01 14:27:49 +1100

    Add .env logic to Deno script

--- lib/scripts/setup-deno.sh
+++ lib/scripts/setup-deno.sh
@@ -10,6 +10,17 @@ echo "----------"
 echo "Changing directory ..."
 cd /var/www/$domain
 
+# SET UP SECRETS
+
+if [ -f ".env.example" ]; then
+  echo "----------"
+  echo "Enter the production environment variables for .env file."
+  echo "Use the following format:"
+  cat .env.example
+  read -s env_vars
+  echo $env_vars >> .env
+fi
+
 # PM2
 
 if [ -f "ecosystem.config.js" ]; then
