4585a180108ece617ed7b2de8fb885357f0a31d3
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2025-12-01 13:08:44 +1100

    Configure permissions on /var/log/$domain

--- lib/scripts/setup-rails.sh
+++ lib/scripts/setup-rails.sh
@@ -10,25 +10,6 @@ echo "----------"
 echo "Changing directory ..."
 cd /var/www/$domain
 
-# LOGS
-
-# Rails, Puma, Nginx and Bullet should log to /var/log and not log/ in Rails root.
-
-# Create directory for logging to
-sudo mkdir /var/log/$domain
-
-# Never keeps logs older than 7 days
-sudo tee /etc/logrotate.d/api.piccolo.floatplane.dev > /dev/null <<EOF
-/var/log/api.piccolo.floatplane.dev/*.log {
-    daily
-    rotate 7
-    compress
-    missingok
-    notifempty
-    copytruncate
-}
-EOF
-
 # DEPLOY USER
 
 # IMPORTANT: 
@@ -63,6 +44,33 @@ getfacl /var/log/$domain/
 git config --global --add safe.directory /var/www/$domain
 
 echo "✅ Done"
+
+# LOGS
+
+# Rails, Puma, Nginx and Bullet should log to /var/log and not log/ in Rails root.
+
+# Create directory for logging to
+sudo mkdir /var/log/$domain
+
+# Make the deploy user owner of the logs
+sudo chown -R $deploy:$deploy /var/log/$domain
+sudo chmod 775 /var/log/$domain
+
+# Allow Nginx to also write to this directory
+sudo usermod -aG $deploy www-data
+sudo systemctl restart nginx
+
+# Never keeps logs older than 7 days
+sudo tee /etc/logrotate.d/api.piccolo.floatplane.dev > /dev/null <<EOF
+/var/log/api.piccolo.floatplane.dev/*.log {
+    daily
+    rotate 7
+    compress
+    missingok
+    notifempty
+    copytruncate
+}
+EOF
 
 # POSTGRES
 
