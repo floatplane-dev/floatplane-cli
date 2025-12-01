ccf6befee61f90c14c184ba5c3e7f08138c6a7d3
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2025-12-01 15:24:00 +1100

    Create git repo for deploy user

--- lib/scripts/setup-github.sh
+++ lib/scripts/setup-github.sh
@@ -3,6 +3,7 @@
 set -eou pipefail
 
 domain=$1
+deploy=$2
 
 wait_until_yes() {
     while true; do
@@ -18,45 +19,54 @@ wait_until_yes() {
 
 echo "----------"
 echo "Setting up Github code repository 🦑"
-
-# Generate SSH key, if it does not yet exist
-if [ ! -f ~/.ssh/readonly@$domain ]; then
-  echo "----------"
-  echo "Generating new SSH key ..."
-  ssh-keygen -t rsa -b 4096 -C "readonly@$domain" -f ~/.ssh/readonly@$domain -P ""
-  cat ~/.ssh/readonly@$domain.pub
-  echo "----------"
-  echo "ACTION REQUIRED:"
-  echo "1. Please copy the public key above."
-  echo "2. Open Github and go to the repository of $domain."
-  echo "3. Add public key as read-only deploy key."
-  echo "4. Done?"
-  wait_until_yes
-fi
-
-# Clone the repo, if it does not yet exist
-if [ ! -d /var/www/$domain ]; then
-  echo "----------"
-  echo "What's the Github SSH URL of this project? 🦑 (git@github.com:floatplane-dev/some-project.git)"
-  while true; do
-    read repo
-    if [[ $repo == git@github.com:* ]]; then
-      break;
-    else
-      echo "Please enter a URL which starts with git@github.com:."
-    fi
-  done
-  echo "----------"
-  echo "Cloning repo ..."
-  GIT_SSH_COMMAND="ssh -i ~/.ssh/readonly@$domain" git clone $repo /var/www/$domain
-  echo "----------"
-  echo "Configuring Git SSH to use the readonly SSH key ..."
-  cd /var/www/$domain
-  git config core.sshCommand "ssh -i ~/.ssh/readonly@$domain -F /dev/null"
-fi
-
+echo "----------"
+echo "Enter the Github SSH URL (git@github.com:floatplane-dev/some-project.git):"
+while true; do
+  read repo
+  if [[ $repo == git@github.com:* ]]; then
+    break;
+  else
+    echo "Please enter a URL which starts with git@github.com:."
+  fi
+done
+echo "----------"
+echo "Generating SSH key for deploy user ..."
+sudo mkdir -p /home/$deploy/.ssh
+sudo ssh-keygen -t rsa -b 4096 -C "$deploy@$domain" -f /home/$deploy/.ssh/$deploy@$domain -P ""
+sudo chown -R $deploy:$deploy /home/$deploy/.ssh
+echo "----------"
+sudo cat /home/$deploy/.ssh/$deploy@$domain.pub
+echo "----------"
+echo "ACTION REQUIRED:"
+echo "1. Copy the public key above"
+echo "2. Open Github and go to the repository of $domain"
+echo "3. Add public key as read-only deploy key"
+echo "4. Done?"
+wait_until_yes
+echo "----------"
+echo "Creating new Git repo ..."
+mkdir /var/www/$domain
+cd /var/www/$domain
+git init --initial-branch=main
+git remote add origin $repo
+echo "✅ done"
+echo "----------"
+echo "Configuring Git to use the new SSH key ..."
+git config core.sshCommand "ssh -i /home/$deploy/.ssh/$deploy@$domain -F /dev/null"
+echo "✅ done"
+echo "----------"
+echo "Make deploy user owner of project root ..."
+sudo chown -R $deploy:$deploy /var/www/$domain
+echo "✅ done"
 echo "----------"
 echo "Pulling latest production code ..."
-cd /var/www/$domain
-git checkout production
-git pull
+sudo -u $deploy bash -lc 'git fetch'
+echo "✅ done"
+echo "----------"
+echo "Checking out production ..."
+sudo -u $deploy bash -lc 'git checkout production'
+echo "✅ done"
+echo "----------"
+# echo "Setting upstream out production ..."
+# sudo -u $deploy bash -lc 'git branch --set-upstream-to=origin/production production'
+# echo "✅ done"
