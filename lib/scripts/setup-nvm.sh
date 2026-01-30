628e48740c1d43ca5c908305b73568c185255eb1
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-01-30 19:12:58 +1100

    Remove unused line

--- lib/scripts/setup-nvm.sh
+++ lib/scripts/setup-nvm.sh
@@ -19,9 +19,6 @@ fi
 # Note: `which nvm` returns nothing because NVM is not an executable binary.
 # It's a shell function defined in ~/.nvm/nvm.sh.
 
-
-if sudo -u $deploy bash -lc 'which pm2' 2>/dev/null | grep -Fx "/home/$deploy/.bun/bin/pm2" >/dev/null; then
-
 if sudo -u interflux bash -lc 'command -v nvm' | grep -Fxq "nvm"; then
     echo "----------"
     echo "✅ NVM is already installed"
