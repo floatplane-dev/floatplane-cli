98d3a6ddb673f3fd341c59bb086b9f15555a4d3c
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-01-30 17:20:43 +1100

    Set up /var/log directory with shared permissions

--- lib/scripts/setup-logs.sh
+++ lib/scripts/setup-logs.sh
@@ -7,26 +7,35 @@ deploy=$2
 
 # LOGS
 
-# Rails, Puma, Nginx and Bullet should log to /var/log and not log/ in Rails root.
+# Rails, Puma, Nginx, Bullet and PM2 all should log to /var/log and not to log/ in project root.
 
-# Create directory for logging to
-sudo mkdir /var/log/$domain
+if [[ -d "/var/log/$domain" ]]; then
+    echo "----------"
+    echo "✅ /var/log/$domain already exists"
+    echo "----------"
+else
+    echo "----------"
+    echo "Creating /var/log/$domain"
+    echo "----------"
 
-# Make the deploy user owner of the logs
-sudo chown -R $deploy:$deploy /var/log/$domain
-sudo chmod 775 /var/log/$domain
+    # Create directory for logging to
+    sudo mkdir /var/log/$domain
 
-# Grant admin access to the logs
-sudo setfacl -R -m u:admin:rwx /var/log/$domain/
-sudo setfacl -R -d -m u:admin:rwx /var/log/$domain/
-sudo getfacl /var/log/$domain/
+    # Make the deploy user owner of the logs
+    sudo chown -R $deploy:$deploy /var/log/$domain
+    sudo chmod 775 /var/log/$domain
 
-# Allow Nginx to also write logs
-sudo usermod -aG $deploy www-data
-sudo systemctl restart nginx
+    # Grant admin access to the logs
+    sudo setfacl -R -m u:admin:rwx /var/log/$domain/
+    sudo setfacl -R -d -m u:admin:rwx /var/log/$domain/
+    sudo getfacl /var/log/$domain/
 
-# Never keeps logs older than 7 days
-sudo tee /etc/logrotate.d/$domain > /dev/null <<EOF
+    # Allow Nginx to also write logs
+    sudo usermod -aG $deploy www-data
+    sudo systemctl restart nginx
+
+    # Never keeps logs older than 7 days
+    sudo tee /etc/logrotate.d/$domain > /dev/null <<EOF
 /var/log/$domain/*.log {
     daily
     rotate 7
@@ -36,3 +45,8 @@ sudo tee /etc/logrotate.d/$domain > /dev/null <<EOF
     copytruncate
 }
 EOF
+
+    echo "----------"
+    echo "✅ Created /var/log/$domain"
+    echo "----------"
+fi
