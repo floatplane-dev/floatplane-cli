11d9ad498ff1d1c73448f11b617e9c31e0abc02a
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-09-11 09:38:42 +1000

    Improve Rails setup so it can run multiple times in a row

--- lib/scripts/setup-rails.sh
+++ lib/scripts/setup-rails.sh
@@ -7,58 +7,84 @@ DEPLOY_USER=bot
 
 echo "----------"
 echo "Setting up Rails 🛤️ ..."
+
+# PACKAGES
+
 echo "----------"
-echo "Changing directory ..."
-cd /var/www/$DOMAIN
-echo "✅ Done"
-
-# POSTGRES
-
+echo "Refreshing local package catalog ..."
+sudo apt update
 echo "----------"
 echo "Installing Postgres ..."
 sudo apt install -y postgresql postgresql-contrib libpq-dev
 echo "✅ Done"
-
-# POSTGRES USER
-
-echo "----------"
-echo "Creating Postgres user named \"$DEPLOY_USER\" ..."
-sudo -u postgres createuser -s $DEPLOY_USER
-echo "✅ Done"
-echo "----------"
-echo "Enter Postgres user password:"
-echo "👉🏼 Store this in 1Password"
-echo "👉🏼 Store this in config/credentials/production.yml.enc"
-read -s DB_PASS
-sudo -u postgres psql -c "ALTER USER $DEPLOY_USER WITH PASSWORD '$DB_PASS';"
-echo "✅ Done"
-
-# RBENV
-
 echo "----------"
 echo "Installing missing packages for compiling Ruby ..."
 sudo apt install -y build-essential libssl-dev libreadline-dev zlib1g-dev libtool libyaml-dev
 echo "✅ Done"
 
-echo "----------"
-echo "Installing rbenv for deploy user ..."
-sudo -u $DEPLOY_USER git clone https://github.com/rbenv/rbenv.git /home/$DEPLOY_USER/.rbenv
-# We should run `rbenv init`, but fails.
-# Instead we add manually what `rbenv init` would have done.
-sudo -u $DEPLOY_USER bash -c 'echo "eval \"\$(~/.rbenv/bin/rbenv init - --no-rehash bash)\"" > /home/$DEPLOY_USER/.bash_profile'
-echo "✅ Done"
+# DIRECTORY
 
 echo "----------"
-echo "Installing Rbenv plugins for deploy user ..."
-sudo -u $DEPLOY_USER mkdir -p /home/$DEPLOY_USER/.rbenv/plugins
-sudo -u $DEPLOY_USER git clone https://github.com/rbenv/ruby-build.git /home/$DEPLOY_USER/.rbenv/plugins/ruby-build
-sudo -u $DEPLOY_USER git clone https://github.com/rbenv/rbenv-vars.git /home/$DEPLOY_USER/.rbenv/plugins/rbenv-vars
+echo "Changing directory ..."
+cd /var/www/$DOMAIN
 echo "✅ Done"
+
+# RBENV
+
+if ! sudo -u $DEPLOY_USER test -d /home/$DEPLOY_USER/.rbenv; then
+    echo "----------"
+    echo "Cloning rbenv ..."
+    sudo -u $DEPLOY_USER git clone https://github.com/rbenv/rbenv.git /home/$DEPLOY_USER/.rbenv
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ rbenv has already been cloned, skipping"
+fi
+
+if ! sudo -u $DEPLOY_USER grep -q 'rbenv init' /home/$DEPLOY_USER/.bash_profile 2>/dev/null; then
+    echo "----------"
+    echo "Adding rbenv to PATH ..."
+
+    # Normally running `rbenv init` should be sufficient, but fails here.
+    # Therefor we manually do what `rbenv init` would have done to make `rbenv` available in PATH.
+
+    sudo -u $DEPLOY_USER bash -c 'echo "eval \"\$(~/.rbenv/bin/rbenv init - --no-rehash bash)\"" >> ~/.bash_profile'
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ rbenv is available in PATH, skipping"
+fi
+
+# RUBY BUILD
+
+if ! sudo -u $DEPLOY_USER test -d /home/$DEPLOY_USER/.rbenv/plugins/ruby-build; then
+    echo "----------"
+    echo "Installing ruby-build plugin for deploy user ..."
+    sudo -u $DEPLOY_USER mkdir -p /home/$DEPLOY_USER/.rbenv/plugins
+    sudo -u $DEPLOY_USER git clone https://github.com/rbenv/ruby-build.git /home/$DEPLOY_USER/.rbenv/plugins/ruby-build
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ ruby-build already exists, skipping"
+fi
+
+# RBENV VARS
+
+if ! sudo -u $DEPLOY_USER test -d /home/$DEPLOY_USER/.rbenv/plugins/rbenv-vars; then
+    echo "----------"
+    echo "Installing rbenv-vars plugin for deploy user ..."
+    sudo -u $DEPLOY_USER mkdir -p /home/$DEPLOY_USER/.rbenv/plugins
+    sudo -u $DEPLOY_USER git clone https://github.com/rbenv/rbenv-vars.git /home/$DEPLOY_USER/.rbenv/plugins/rbenv-vars
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ rbenv-vars already exists, skipping"
+fi
 
 # RUBY
 
 echo "----------"
-echo "Installing Ruby for deploy user ..."
+echo "Installing Ruby ..."
 rubyversion=$(cat .ruby-version)
 echo $rubyversion
 # On Mac you can simply run `rbenv install`. On Debian we must specify the exact version.
@@ -72,24 +98,29 @@ echo "✅ Done"
 # Until Bundler 2.3 we need to install the exact version ourselves.
 
 echo "----------"
-echo "Installing Bundler v$bundlerversion for deploy user ..."
+echo "Installing Bundler v$bundlerversion ..."
 sudo -u $DEPLOY_USER bash -lc "gem install bundler -v \"$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)\""
 echo "✅ Done"
 
 # GEMS
 
 echo "----------"
-echo "Installing gems for deploy user ..."
+echo "Installing gems ..."
 sudo -u $DEPLOY_USER bash -lc "bundle install"
 echo "✅ Done"
 
 # SECRETS
 
-echo "----------"
-echo "Enter the config/credentials/production.key:"
-read -s PRODUCTION_KEY
-echo $PRODUCTION_KEY >> config/credentials/production.key
-echo "✅ Done"
+if [ ! -f config/credentials/production.key ]; then
+    echo "----------"
+    echo "Enter the config/credentials/production.key:"
+    read -s PRODUCTION_KEY
+    echo $PRODUCTION_KEY >> config/credentials/production.key
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ production.key already exists, skipping"
+fi
 
 # ENVIRONMENT
 
@@ -101,35 +132,70 @@ echo "✅ Done"
 # * bin/rails db:migrate
 # * bin/puma -C config/puma.rb
 
-echo "----------"
-echo "Setting RAILS_ENV=production on rbenv-vars"
-echo "RAILS_ENV=production" >> .rbenv-vars
-echo "✅ Done"
+if ! grep -q '^RAILS_ENV=' .rbenv-vars 2>/dev/null; then
+    echo "----------"
+    echo "Setting RAILS_ENV=production on rbenv-vars"
+    echo "RAILS_ENV=production" >> .rbenv-vars
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ RAILS_ENV already set, skipping"
+fi
+
+# POSTGRES USER
+
+if ! sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='$DEPLOY_USER'" | grep -q 1; then
+    echo "----------"
+    echo "Creating Postgres user named \"$DEPLOY_USER\" ..."
+    sudo -u postgres createuser -s $DEPLOY_USER
+    echo "✅ Done"
+    echo "----------"
+    echo "Enter Postgres user password:"
+    echo "👉🏼 Store this in 1Password"
+    echo "👉🏼 Store this in config/credentials/production.yml.enc"
+    read -s DB_PASS
+    sudo -u postgres psql -c "ALTER USER $DEPLOY_USER WITH PASSWORD '$DB_PASS';"
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ Postgres user \"$DEPLOY_USER\" already exists, skipping"
+fi
 
 # CREATE DATABASE
 
 echo "----------"
 echo "Creating database ..."
-sudo -u $DEPLOY_USER bash -lc "bin/rails db:create"
-echo "✅ Done"
+sudo -u $DEPLOY_USER bash -lc "bin/rails db:prepare"
 
-# DATABASE SCHEMA
+# NOTE:
+# rails db:prepare is a shorthand for:
+# rails db:create && rails db:schema:load` if database is absent
+# rails:db:migrate if database is present
 
-echo "----------"
-echo "Apply database schema..."
-sudo -u $DEPLOY_USER bash -lc "bin/rails db:schema:load"
 echo "✅ Done"
 
 # PUMA
 
-echo "----------"
-echo "Configuring Puma service"
-sudo ln -s /var/www/$DOMAIN/config/puma.service /etc/systemd/system/$DEPLOY_USER.service
-sudo systemctl daemon-reload
-echo "✅ Done"
+if [ ! -e /etc/systemd/system/$DOMAIN.service ]; then
+    echo "----------"
+    echo "Sym linking Puma to systemd ..."
+    sudo ln -s /var/www/$DOMAIN/config/puma.service /etc/systemd/system/$DOMAIN.service
+    echo "✅ Done"
+else
+    echo "----------"
+    echo "✅ Puma is already sym linked to systemd, skipping"
+fi
 
 echo "----------"
-echo "Starting Puma service"
-sudo systemctl start $DEPLOY_USER
-sudo systemctl status $DEPLOY_USER --no-pager
+echo "Stopping Puma service ... (if any)"
+sudo systemctl stop $DOMAIN
+echo "----------"
+echo "Reloading daemons (for possible changes)"
+sudo systemctl daemon-reload
+echo "----------"
+echo "Starting Puma service ..."
+sudo systemctl start $DOMAIN
+echo "----------"
+echo "Status Puma service:"
+sudo systemctl status $DOMAIN --no-pager
 echo "✅ Done"
